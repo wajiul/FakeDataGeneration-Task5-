138e1f6195761eda8500b6bee48edb0d9f65a5e5
Owajeul Islam <owajeul01357@gmail.com>
2024-07-14 22:18:18 +0600

    add ErrorGenerarorService

---
+++ Services/ErrorGeneratorService.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+
+namespace FakeDataGeneration.Services
+{
+    public class ErrorGeneratorService
+    {
+        private Random rng = new Random(1234);
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        private string DeleteCharacter(string data)
+        {
+            if (data.Length == 0)
+                return data;
+            int pos = rng.Next(0, data.Length);
+            return data.Remove(pos, 1);
+        }
+
+        private string AddRandomCharacter(string data)
+        {
+            int pos = rng.Next(0, data.Length + 1);
+            char randomChar = GetRandomCharacter();
+            return data.Insert(pos, randomChar.ToString());
+        }
+
+        private string SwapCharacters(string data)
+        {
+            if (data.Length < 2)
+                return data;
+            int pos = rng.Next(0, data.Length - 1);
+            var sb = new StringBuilder(data);
+            char temp = sb[pos];
+            sb[pos] = sb[pos + 1];
+            sb[pos + 1] = temp;
+            return sb.ToString();
+        }
+
+        private char GetRandomCharacter()
+        {
+            return chars[rng.Next(chars.Length)];
+        }
+
+        private string AddErrorToColumn(string data)
+        {
+            Func<string, string>[] errorMethods = { DeleteCharacter, AddRandomCharacter, SwapCharacters };
+            var errorMethod = errorMethods[rng.Next(errorMethods.Length)];
+            return errorMethod(data);
+        }
+        public List<string> AddErrors(List<string> data, double numErrors, int skipColumn = 2)
+        {
+            numErrors = (rng.NextDouble() < numErrors % 1) ? numErrors + 1 : numErrors;
+
+            for (int i = 0; i < Math.Floor(numErrors); i++)
+            {
+                int columnIndex = rng.Next(skipColumn, data.Count);
+                string column = data[columnIndex];
+                data[columnIndex] = AddErrorToColumn(column);
+            }
+            return data;
+        }
+
+    }
+}
